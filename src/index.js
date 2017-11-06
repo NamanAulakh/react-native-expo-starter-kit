c1f8f532b173d754056b0025e4f79547ae5bd094
Naman <aulakh.naman@gmail.com>
2017-11-06 17:38:18 +0530

    husky test

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class Root extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Yo</Text>
+        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Yo...</Text>
       </View>
     )
   }
